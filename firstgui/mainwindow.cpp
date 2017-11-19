cec09bff194d5e7e91b7dd06f2047e09e287b6c9
macro gu <macrogu@qq.com>
2017-11-19 09:50:54 -0600

    1. add button operation

--- firstgui/mainwindow.cpp
+++ firstgui/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -10,10 +11,32 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->horizontalSlider, SIGNAL(valueChanged(int)),
                ui->progressBar, SLOT(setValue(int)));
 
-
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+//    void about
+//    void aboutQt
+//    StandardButton critical
+//    StandardButton information
+//    StandardButton question
+//    StandardButton warning
+    QMessageBox::StandardButton reply = QMessageBox::question(this,
+                          "My Title","this is question? ",
+                          QMessageBox::Ok|QMessageBox::No);
+    if (reply == QMessageBox::Yes)
+    {
+        QApplication::quit();
+    }
+    else
+    {
+        qDebug("No is clicked.");
+    }
+
+
 }
