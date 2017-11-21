b2f7f1668f9006458b0385c6d8c65d7c3fb6d273
macro gu <macrogu@qq.com>
2017-11-21 16:51:48 -0600

    add QTabWidget

--- firstgui/mainwindow.h
+++ firstgui/mainwindow.h
@@ -16,27 +16,10 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-
 private slots:
-    void on_actionNew_triggered();
+    void on_tabWidget_tabCloseRequested(int index);
 
-    void on_actionOpen_triggered();
-
-    void on_actionSave_triggered();
-
-    void on_actionSave_as_triggered();
-
-    void on_actionCut_triggered();
-
-    void on_actionCopy_triggered();
-
-    void on_actionPaste_triggered();
-
-    void on_actionRedo_triggered();
-
-    void on_actionUndo_triggered();
-
-    void on_actionAbout_Notepad_triggered();
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
