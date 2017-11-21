d9f4268aac299266358a9083e78c652522c98bf6
macro gu <macrogu@qq.com>
2017-11-20 18:05:12 -0600

    add combo box

--- firstgui/mainwindow.cpp
+++ firstgui/mainwindow.cpp
@@ -8,6 +8,16 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+//    ui->comboBox->addItem(QIcon(":/img/img/open.jpg"),"Mark");
+//    ui->comboBox->addItem(QIcon(":/img/img/open.jpg"),"Jhon");
+//    ui->comboBox->addItem(QIcon(":/img/img/1.jpg"),"July");
+
+    for (int i = 0; i < 10; i++)
+    {
+        ui->comboBox->addItem(QIcon(":/img/img/open.jpg"), QString::number(i) + "name");
+    }
+
+    ui->comboBox->insertItem(3, QIcon(":/img/img/1.jpg"), "New Text");
 }
 
 MainWindow::~MainWindow()
@@ -15,17 +25,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_actionNew_triggered()
+void MainWindow::on_pushButton_clicked()
 {
-    QMessageBox::information(this, "title", "New");
-}
-
-void MainWindow::on_actionOpen_triggered()
-{
-     QMessageBox::information(this, "title", "Open");
-}
-
-void MainWindow::on_actionExit_triggered()
-{
-    QApplication::quit();
+    QMessageBox::information(this, "title", QString::number(ui->comboBox->currentIndex()));
 }
